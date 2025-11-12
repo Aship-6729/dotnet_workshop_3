e4aa1c82cb7f329706afba5f34d949ef3d03e853
Aship-6729 <aalamaship98@gmail.com>
2025-11-12 11:46:05 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace Task_1
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Operator.Add(10, 5);
+            Operator.Subtract(10, 5);
+            Operator.Multiply(10, 5);
+            Operator.Divide(10, 5);
+            Operator.OddEvenFinder(10);
+            Operator.OddEvenFinder(7);
+
+        }
+
+        
+    }
+}
