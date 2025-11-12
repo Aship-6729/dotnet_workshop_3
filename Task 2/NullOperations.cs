7973ee3256bdbf85a2107ab8a75560308636c8c5
Aship-6729 <aalamaship98@gmail.com>
2025-11-12 12:17:57 +0545

    Task 2

---
+++ Task 2/NullOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    internal class NullOperations
+    {
+        public static void PerformNullCheck()
+        {
+
+            string username = null;
+            string value1 = (username == null ? "Username is not available" : $"Username is {username}");
+            Console.WriteLine(value1);
+        
+            
+            string value2 = username ?? "Username is not available"; // if username is null it will return the message only.
+            //Console.WriteLine($"Null-coalescing Operators Username {value2}");
+
+            username ??= "DefaultUser"; // if user is null it will assign as a defauluser.
+            Console.WriteLine($"After Null-coalescing assignment operator Username is {username}");
+        }
+    }
+}
--- Task 2/obj/Debug/net9.0/Task 2.AssemblyInfo.cs
+++ Task 2/obj/Debug/net9.0/Task 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4aa1c82cb7f329706afba5f34d949ef3d03e853")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
